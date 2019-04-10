a7cef65f315cfc0ad4a88c68ee6e9f15320320c8
David De Groot <david.degroot@softtechconsulting.com>
2019-04-10 14:18:47 -0400

    EGIS-35: Adding tests for biodata.

--- src/test/java/com/softtech/cloud/egisexampleconsumer/EgisExampleConsumerApplicationTests.java
+++ src/test/java/com/softtech/cloud/egisexampleconsumer/EgisExampleConsumerApplicationTests.java
@@ -1,5 +1,6 @@
 package com.softtech.cloud.egisexampleconsumer;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 import java.util.Map;
@@ -11,7 +12,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
 import org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties.StubsMode;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestOperations;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -19,7 +23,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.MOCK)
-@AutoConfigureStubRunner(repositoryRoot = "http://nexus-ci.apps.softtechdemos.com/repository/maven-releases", classifier = "stubs", stubsMode = StubsMode.REMOTE, ids = "com.softtech:egis-visa-applicant-stubs:test-upload-rel-1", minPort = 12000, maxPort = 12000)
+@AutoConfigureStubRunner(repositoryRoot = "http://nexus-ci.apps.softtechdemos.com/repository/maven-releases", classifier = "stubs", stubsMode = StubsMode.REMOTE, ids = "com.softtech:egis-visa-applicant-stubs:test-upload-rel-6", minPort = 12000, maxPort = 12000)
 public class EgisExampleConsumerApplicationTests {
 
 	@Autowired
@@ -36,5 +40,45 @@ public class EgisExampleConsumerApplicationTests {
 		assertNotNull(returnedVal);
 		final String jsonForm = new ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(returnedVal);
 		System.out.println(jsonForm);
+	}
+	
+	@Test
+	public void testingVisaSummaryGetByBirthdateBioData() throws Exception {
+		assertNotNull(restOperations);
+		final String uriString = UriComponentsBuilder.fromUriString("http://localhost:12000/visa/v1/bioData")
+				.queryParam("birthDate", "1985-01-01").build().toUriString();
+
+		@SuppressWarnings("unchecked")
+		ResponseEntity<Map> response = restOperations.getForEntity(uriString, Map.class);
+		assertNotNull(response);
+		assertEquals(HttpStatus.OK.value(), response.getStatusCodeValue());
+		final String jsonForm = new ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(response.getBody());
+		System.out.println(jsonForm);
+	}
+	
+	@Test
+	public void testingVisaSummaryGetByBirthdateAndNameBioData() throws Exception {
+		assertNotNull(restOperations);
+		final String uriString = UriComponentsBuilder.fromUriString("http://localhost:12000/visa/v1/bioData")
+				.queryParam("birthDate", "1985-01-01")
+				.queryParam("lastName", "Tracer").build().toUriString();
+
+		@SuppressWarnings("unchecked")
+		ResponseEntity<Map> response = restOperations.getForEntity(uriString, Map.class);
+		assertNotNull(response);
+		assertEquals(HttpStatus.OK.value(), response.getStatusCodeValue());
+		final String jsonForm = new ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(response.getBody());
+		System.out.println(jsonForm);
+	}
+	
+	@Test(expected = HttpClientErrorException.class)
+	public void testingVisaSummaryGetByBirthdateAndNameBioData_noDataFound() throws Exception {
+		assertNotNull(restOperations);
+		final String uriString = UriComponentsBuilder.fromUriString("http://localhost:12000/visa/v1/bioData")
+				.queryParam("birthDate", "1985-01-01")
+				.queryParam("lastName", "Tracker").build().toUriString();
+
+		@SuppressWarnings("unchecked")
+		ResponseEntity<Map> response = restOperations.getForEntity(uriString, Map.class);
 	}
 }
